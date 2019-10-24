7a2b35f37001e4e0efa11dd36fe12d91c384e2ab
Jayanath Liyanage <jayanath1987@gmail.com>
2019-10-24 16:56:17 +0530

    Add files via upload

---
+++ procedureroom/controllers/procedureroom.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+--------------------------------------------------------------------------------
+HHIMS - Hospital Health Information Management System
+Copyright (c) 2011 Information and Communication Technology Agency of Sri Lanka
+<http: www.hhims.org/>
+----------------------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along 
+with this program. If not, see <http://www.gnu.org/licenses/> 
+
+
+
+
+---------------------------------------------------------------------------------- 
+Date : June 2016
+Author: Mr. Jayanath Liyanage   jayanathl@icta.lk
+
+Programme Manager: Shriyananda Rathnayake
+URL: http://www.govforge.icta.lk/gf/project/hhims/
+----------------------------------------------------------------------------------
+*/
+class Procedureroom extends MX_Controller {
+	 function __construct(){
+		parent::__construct();
+		$this->checkLogin();
+		$this->load->library('session');
+		if(isset($_GET["mid"])){
+			$this->session->set_userdata('mid', $_GET["mid"]);
+		}			
+	 }
+
+	public function index()
+	{
+		$new_page   =   base_url()."index.php/search/procedures_order/";
+		header("Status: 200");
+		header("Location: ".$new_page);
+	}
+	
+} 
+
+
+//////////////////////////////////////////
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
